2143771f7ce44c81165c22c7e62da0e7e9a2ffca
Arudovwen <successahon@gmail.com>
2020-05-06 01:39:18 +0100

    update site

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,24 @@ var main = document.getElementById('main')
 Array.from(image).forEach((element) => {
   element.addEventListener("click", (el) => {
     siteimage.src = el.srcElement.src;
+
+   
     imagename.innerHTML =  el.srcElement.alt
+    if (el.srcElement.alt == "bizguruh website") {
+        imagename.href = 'https://bizguruh.com'
+    }
+    if (el.srcElement.alt == "Abaytour website") {
+        imagename.href = 'https://a-baytours.com'
+    }
+    if (el.srcElement.alt == "PMS website") {
+        imagename.href = 'https://arudovwen.github.io'
+    }
+    if (el.srcElement.alt == "IMS website") {
+        imagename.href = 'https://ims-ministry.herokuapp.com'
+    }
+    if (el.srcElement.alt == "Memory match website") {
+        imagename.href = 'https://brolik-pms.herokuapp.com'
+    }
     overlay.style.display = "flex";
     overlay.style.zIndex = 3;
     main.style.height = '100vh'
